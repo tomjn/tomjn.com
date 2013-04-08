fcd66e8a29a6118de3bc4c868edec1c233ac6feb
Tom J Nowell <contact@tomjn.com>
2013-04-08 12:47:37 +0100

    modify font stack to use crete round when typekit fails

--- functions.php
+++ functions.php
@@ -14,90 +14,90 @@
 if ( ! isset( $content_width ) )
 	$content_width = 640; /* pixels */
 
-if ( ! function_exists( 'tomjnsetup' ) ):
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which runs
- * before the init hook. The init hook is too late for some features, such as indicating
- * support post thumbnails.
- *
- * @since tomjn 1.0
- */
-function tomjnsetup() {
-
+if ( ! function_exists( 'tomjnsetup' ) ) {
 	/**
-	 * Custom template tags for this theme.
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * Note that this function is hooked into the after_setup_theme hook, which runs
+	 * before the init hook. The init hook is too late for some features, such as indicating
+	 * support post thumbnails.
+	 *
+	 * @since tomjn 1.0
 	 */
-	require( get_template_directory() . '/inc/template-tags.php' );
+	function tomjnsetup() {
 
-	/**
-	 * Custom functions that act independently of the theme templates
-	 */
-	//require( get_template_directory() . '/inc/tweaks.php' );
+		/**
+		 * Custom template tags for this theme.
+		 */
+		require( get_template_directory() . '/inc/template-tags.php' );
 
-	/**
-	 * Custom Theme Options
-	 */
-	//require( get_template_directory() . '/inc/theme-options/theme-options.php' );
+		/**
+		 * Custom functions that act independently of the theme templates
+		 */
+		//require( get_template_directory() . '/inc/tweaks.php' );
 
-	/**
-	 * WordPress.com-specific functions and definitions
-	 */
-	//require( get_template_directory() . '/inc/wpcom.php' );
+		/**
+		 * Custom Theme Options
+		 */
+		//require( get_template_directory() . '/inc/theme-options/theme-options.php' );
 
-	/**
-	 * Make theme available for translation
-	 * Translations can be filed in the /languages/ directory
-	 * If you're building a theme based on tomjn, use a find and replace
-	 * to change 'tomjn' to the name of your theme in all the template files
-	 */
-	load_theme_textdomain( 'tomjn', get_template_directory() . '/languages' );
+		/**
+		 * WordPress.com-specific functions and definitions
+		 */
+		//require( get_template_directory() . '/inc/wpcom.php' );
 
-	/**
-	 * Add default posts and comments RSS feed links to head
-	 */
-	add_theme_support( 'automatic-feed-links' );
+		/**
+		 * Make theme available for translation
+		 * Translations can be filed in the /languages/ directory
+		 * If you're building a theme based on tomjn, use a find and replace
+		 * to change 'tomjn' to the name of your theme in all the template files
+		 */
+		load_theme_textdomain( 'tomjn', get_template_directory() . '/languages' );
 
-	/**
-	 * Enable support for Post Thumbnails
-	 */
-	add_theme_support( 'post-thumbnails' );
+		/**
+		 * Add default posts and comments RSS feed links to head
+		 */
+		add_theme_support( 'automatic-feed-links' );
 
-	/**
-	 * This theme uses wp_nav_menu() in one location.
-	 */
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'tomjn' ),
-	) );
+		/**
+		 * Enable support for Post Thumbnails
+		 */
+		add_theme_support( 'post-thumbnails' );
 
-	/**
-	 * Add support for the Aside Post Formats
-	 */
-	add_theme_support( 'post-formats', array( 'aside', ) );
+		/**
+		 * This theme uses wp_nav_menu() in one location.
+		 */
+		register_nav_menus( array(
+			'primary' => __( 'Primary Menu', 'tomjn' ),
+		) );
 
-	if(function_exists('register_template')){
+		/**
+		 * Add support for the Aside Post Formats
+		 */
+		add_theme_support( 'post-formats', array( 'aside', ) );
 
-		register_template( 'twin-column-pages', array( 'post_types' => array( 'page' ) ) );
+		if ( function_exists( 'register_template' ) ) {
 
-		register_template_sidebar( 'Left Sidebar', 'twin-column-pages', array(
-														'description' => 'Just a test',
-														'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-														'after_widget' => "</aside>\n",
-														'before_title' => '<h3 class="widgettitle">',
-														'after_title' => "</h3>\n"
-													) );
+			register_template( 'twin-column-pages', array( 'post_types' => array( 'page' ) ) );
 
-		register_template_sidebar( 'Right Sidebar', 'twin-column-pages', array(
-														'description' => 'This is another sidebar',
-														'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-														'after_widget' => "</aside>\n",
-														'before_title' => '<h3 class="widgettitle">',
-														'after_title' => "</h3>\n"
-													) );
+			register_template_sidebar( 'Left Sidebar', 'twin-column-pages', array(
+															'description' => 'Just a test',
+															'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+															'after_widget' => "</aside>\n",
+															'before_title' => '<h3 class="widgettitle">',
+															'after_title' => "</h3>\n"
+														) );
+
+			register_template_sidebar( 'Right Sidebar', 'twin-column-pages', array(
+															'description' => 'This is another sidebar',
+															'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+															'after_widget' => "</aside>\n",
+															'before_title' => '<h3 class="widgettitle">',
+															'after_title' => "</h3>\n"
+														) );
+		}
 	}
-}
-endif; // tomjnsetup
+} // tomjnsetup
 add_action( 'after_setup_theme', 'tomjnsetup' );
 
 /**
@@ -140,10 +140,10 @@ function tomjnscripts() {
 add_action( 'wp_enqueue_scripts', 'tomjnscripts' );
 
 function filter_ptags_on_images( $content ) {
-   return preg_replace('/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
+   return preg_replace( '/<p>\s*(<a .*>)?\s*(<img .* \/>)\s*(<\/a>)?\s*<\/p>/iU', '\1\2\3', $content);
 }
 
-add_filter('the_content', 'filter_ptags_on_images');
+add_filter( 'the_content', 'filter_ptags_on_images' );
 
 
 add_action( 'admin_head-upload.php', 'wpse_59182_bigger_media_thumbs' );
@@ -164,17 +164,46 @@ function wpse_59182_bigger_media_thumbs() {
 
 
 function tomjn_typekit_code() {
-?>
-<script type="text/javascript" src="//use.typekit.net/wtc2mfi.js"></script>
-<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-<?php
+	?>
+	<script type="text/javascript" src="//use.typekit.net/wtc2mfi.js"></script>
+	<script type="text/javascript">try{Typekit.load({
+        inactive: function() {
+          // load google fonts instead
+          WebFontConfig = {
+		    google: { families: [ 'Gentium+Book+Basic:400,400italic,700,700italic:latin' ] }
+		  };
+		  (function() {
+		    var wf = document.createElement('script');
+		    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		    wf.type = 'text/javascript';
+		    wf.async = 'true';
+		    var s = document.getElementsByTagName('script')[0];
+		    s.parentNode.insertBefore(wf, s);
+		  })();
+        }
+      });}catch(e){
+		WebFontConfig = {
+		    google: { families: [ 'Crete+Round:400,400italic:latin' ] }
+		  };
+		  (function() {
+		    var wf = document.createElement('script');
+		    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+		      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+		    wf.type = 'text/javascript';
+		    wf.async = 'true';
+		    var s = document.getElementsByTagName('script')[0];
+		    s.parentNode.insertBefore(wf, s);
+		  })();
+		}</script>
+	<?php
 }
 
-add_action('wp_head','tomjn_typekit_code');
+add_action( 'wp_head', 'tomjn_typekit_code' );
 
-add_filter("mce_external_plugins", "tomjn_mce_external_plugins");
-function tomjn_mce_external_plugins($plugin_array){
-	$plugin_array['typekit']  =  get_template_directory_uri().'/typekit.tinymce.js';//('/ilc-syntax-buttons/ilcsyntax.js');
+add_filter( 'mce_external_plugins', 'tomjn_mce_external_plugins' );
+function tomjn_mce_external_plugins( $plugin_array ) {
+	$plugin_array['typekit'] = get_template_directory_uri().'/typekit.tinymce.js';//('/ilc-syntax-buttons/ilcsyntax.js');
     return $plugin_array;
 }
 
@@ -184,20 +213,20 @@ add_editor_style( 'editor-style.less' );
 function add_favicon(){
 	?><link rel="shortcut icon" type="image/png" href="<?php echo home_url(); ?>/favicon.png" /><?php
 }
-add_action('wp_head','add_favicon');
-add_action('admin_head','add_favicon');
+add_action( 'wp_head', 'add_favicon' );
+add_action( 'admin_head', 'add_favicon' );
 
 
 // Add Slideshare oEmbed
 function add_oembed_slideshare(){
-	wp_oembed_add_provider( 'http://www.slideshare.net/*', 'http://api.embed.ly/v1/api/oembed');
+	wp_oembed_add_provider( 'http://www.slideshare.net/*', 'http://api.embed.ly/v1/api/oembed' );
 }
-add_action('init','add_oembed_slideshare');
+add_action( 'init', 'add_oembed_slideshare' );
 
 // Create a new filtering function that will add our where clause to the query
 function password_post_filter( $where = '' ) {
     // Make sure this only applies to loops / feeds on the frontend
-    if (!is_single() && !is_admin()) {
+    if ( !is_single() && !is_admin() ) {
         // exclude password protected
         $where .= " AND post_password = ''";
     }
