0036dc8f9c882d3334cf0ad31b099569737034c2
Tom J Nowell <contact@tomjn.com>
2013-04-18 10:57:56 +0100

    display taxonomies dynamically

--- content.php
+++ content.php
@@ -16,44 +16,43 @@
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
+<?php
+if ( is_search() || is_archive() ) {
+	// Only display Excerpts for Search and archives
+	?>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
-	<?php else : ?>
+	<?php
+} else {
+		?>
 	<div class="entry-content">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', '_s' ) ); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', '_s' ), 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
-	<?php endif; ?>
-
+	<?php
+}
+?>
 	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', '_s' ) );
-				if ( $categories_list && _s_categorized_blog() ) :
-			?>
-			<span class="cat-links">
-				<?php printf( __( 'Posted in %1$s', '_s' ), $categories_list ); ?>
-			</span>
-			<?php endif; // End if categories ?>
-
-			<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ' ', '_s' ) );
-				if ( $tags_list ) :
-			?>
-			<span class="tag-links">
-				<?php printf( __( 'Tagged %1$s', '_s' ), $tags_list ); ?>
-			</span>
-			<?php endif; // End if $tags_list ?>
-		<?php endif; // End if 'post' == get_post_type() ?>
+<?php
+$post_type = get_post_type();
+foreach ( get_object_taxonomies( $post_type ) as $tax_name ) {
+    $term_list = get_the_term_list( get_the_ID(), $tax_name, '', ' ', '' );
+	if ( !empty( $term_list ) ) {
+		$the_tax = get_taxonomy( $tax_name );
+		?>
+		<span class="<?php echo $tax_name; ?>-links">
+			<?php printf( __( '%1$s: %2$s<br>', '_s' ), $the_tax->labels->name, $term_list ); ?>
+		</span>
+		<?php
+	}
+}
+?>
 
 		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
 		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', '_s' ), __( '1 Comment', '_s' ), __( '% Comments', '_s' ) ); ?></span>
 		<?php endif; ?>
 
-		<?php edit_post_link( __( 'Edit', '_s' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', '_s' ), ' <span class="edit-link">', '</span>' ); ?>
 	</footer><!-- #entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
