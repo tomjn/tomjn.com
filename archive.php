10c3e12bf7a4bb0d0605eb474697e7f779e857e1
Tom J Nowell <contact@tomjn.com>
2013-04-22 14:55:45 +0100

    change the archives template to show a sidebar to the left like taxonomy.php

--- archive.php
+++ archive.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying Archive pages.
+ * The template for displaying Taxonomy pages.
  *
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
@@ -8,92 +8,88 @@
  * @since _s 1.0
  */
 
-get_header(); ?>
+get_header();
+$taxonomy = 'category';
+if ( is_tax() ) {
+	$taxonomy = get_query_var( 'taxonomy' );
+} else {
+	if ( is_category() ) {
+		$taxonomy = 'category';
+	} else {
+		$taxonomy = 'post_tag';
+	}
+}
 
+$current_term =	$wp_query->queried_object;
+$tname = $taxonomy;
+$taxobj = get_taxonomy( $taxonomy );
+if ( !empty( $taxobj ) ) {
+	$tname = $taxobj->labels->name;
+}
+?>
 		<section id="primary" class="site-content">
 			<div id="content" role="main">
-
-			<?php if ( have_posts() ) : ?>
-
+<?php
+if ( have_posts() ) {
+	?>
 				<header class="page-header">
-					<h1 class="page-title">
-						<?php
-							if ( is_category() ) {
-								printf( __( 'Category Archives: %s', '_s' ), '<span>' . single_cat_title( '', false ) . '</span>' );
 
-							} elseif ( is_tag() ) {
-								printf( __( 'Tag Archives: %s', '_s' ), '<span>' . single_tag_title( '', false ) . '</span>' );
-
-							} elseif ( is_author() ) {
-								/* Queue the first post, that way we know
-								 * what author we're dealing with (if that is the case).
-								*/
-								the_post();
-								printf( __( 'Author Archives: %s', '_s' ), '<span class="vcard"><a class="url fn n" href="' . get_author_posts_url( get_the_author_meta( "ID" ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' );
-								/* Since we called the_post() above, we need to
-								 * rewind the loop back to the beginning that way
-								 * we can run the loop properly, in full.
-								 */
-								rewind_posts();
-
-							} elseif ( is_day() ) {
-								printf( __( 'Daily Archives: %s', '_s' ), '<span>' . get_the_date() . '</span>' );
-
-							} elseif ( is_month() ) {
-								printf( __( 'Monthly Archives: %s', '_s' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
-
-							} elseif ( is_year() ) {
-								printf( __( 'Yearly Archives: %s', '_s' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
-
-							} else {
-								_e( 'Archives', '_s' );
-
-							}
-						?>
-					</h1>
-					<?php
-						if ( is_category() ) {
-							// show an optional category description
-							$category_description = category_description();
-							if ( ! empty( $category_description ) )
-								echo apply_filters( 'category_archive_meta', '<div class="taxonomy-description">' . $category_description . '</div>' );
-
-						} elseif ( is_tag() ) {
-							// show an optional tag description
-							$tag_description = tag_description();
-							if ( ! empty( $tag_description ) )
-								echo apply_filters( 'tag_archive_meta', '<div class="taxonomy-description">' . $tag_description . '</div>' );
-						}
-					?>
+	<?php
+	echo '<h1>'.$taxobj->labels->singular_name.': '.$current_term->name.'</h1>';
+	//$tag_description = tag_description();
+	//	if ( ! empty( $tag_description ) )
+	//		echo apply_filters( 'tax_archive_meta', '<div class="taxonomy-description">' . $tax_description . '</div>' );
+	?>
 				</header>
+				<div class="taxonomy-list">
+					<div class="taxonomy-term-choices">
+						<h1 class="page-title">
+	<?php
+	echo $tname;
+	?>
+					</h1>
+	<?php
+	$terms = get_terms( array( $taxonomy ) );
+	if ( !empty( $terms ) && !is_wp_error( $terms ) ) {
+		echo '<ul class="taxonomy-term-choices-list">';
+		foreach ( $terms as $term ) {
+			$class = '';
+			if ( $term->slug == $current_term->slug ) {
+				$class = 'active';
+			}
+		    echo '<li class="'.$class.'"><a href="'.get_term_link( $term->slug, $taxonomy ).'">'.$term->name.'</a></li>';
+		}
+		echo '</ul>';
+	}
+	?>
+					</div>
+					<div class="taxonomy-listing">
+	<?php
+	//rewind_posts();
 
-				<?php rewind_posts(); ?>
+//	_s_content_nav( 'nav-above' );
 
-				<?php _s_content_nav( 'nav-above' ); ?>
+	/* Start the Loop */
+	while ( have_posts() ) {
+		the_post();
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+		/* Include the Post-Format-specific template for the content.
+		 * If you want to overload this in a child theme then include a file
+		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+		 */
+		get_template_part( 'content', get_post_type() );
+	}
+	_s_content_nav( 'nav-below' );
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-					?>
-
-				<?php endwhile; ?>
-
-				<?php _s_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<?php get_template_part( 'no-results', 'archive' ); ?>
-
-			<?php endif; ?>
-
+} else {
+	get_template_part( 'no-results', 'archive' );
+}
+?>
+					</div>
+				</div>
 			</div><!-- #content -->
 		</section><!-- #primary .site-content -->
 
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+get_sidebar();
+get_footer();
