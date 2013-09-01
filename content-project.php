90d17a6c8c8c558cdeecb5cdc2659ad97e1fad67
tarendai <tarendai@darkstars.co.uk>
2013-09-01 22:14:59 +0100

    fix p nesting

--- content-project.php
+++ content-project.php
@@ -11,7 +11,7 @@
 		<div class="grid__item  one-whole  lap-one-half  desk-one-half">
 			<header class="entry-header">
 				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<p><?php the_excerpt(); ?></p>
+				<?php the_excerpt(); ?>
 			</header>
 		</div>
 		<div class="grid__item  one-whole  lap-one-half  desk-one-half">
