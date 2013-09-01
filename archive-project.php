5647593320e4af6c75bf788af11c8aee8526d1c6
Tom J Nowell <contact@tomjn.com>
2013-09-01 18:18:12 +0100

    add project post type display, and grid system

---
+++ archive-project.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * The template for displaying Taxonomy pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _s
+ * @since _s 1.0
+ */
+
+get_header();
+$taxonomy = '';
+if ( is_tax() ) {
+	$taxonomy = get_query_var( 'taxonomy' );
+} else {
+	if ( is_category() ) {
+		$taxonomy = 'category';
+	} else if ( is_tag() ) {
+		$taxonomy = 'post_tag';
+	}
+}
+
+$current_term =	$wp_query->queried_object;
+$tname = $taxonomy;
+$taxobj = get_taxonomy( $taxonomy );
+if ( !empty( $taxobj ) ) {
+	$tname = $taxobj->labels->name;
+}
+?>
+		<section id="primary" class="site-content">
+			<div id="content" role="main">
+<?php
+if ( have_posts() ) {
+	?>
+				<header class="page-header">
+
+	<?php
+	echo '<h1>Projects &amp; Work</h1>';
+	?>
+				</header>
+
+					<div class="taxonomy-listing grid">
+	<?php
+	//rewind_posts();
+
+//	_s_content_nav( 'nav-above' );
+
+	/* Start the Loop */
+	while ( have_posts() ) {
+		the_post();
+
+		?>
+		<div class="grid__item one-whole lap-one-half desk-one-half">
+			<div class="grid">
+				<div class="grid__item one-third ">
+					<?php the_post_thumbnail( 'medium' ); ?>
+				</div>
+				<div class="grid__item two-thirds">
+					<h2><?php the_title(); ?></h2>
+					<p><?php the_excerpt(); ?></p>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+	_s_content_nav( 'nav-below' );
+
+} else {
+	get_template_part( 'no-results', 'archive' );
+}
+?>
+					</div>
+				</div>
+			</div><!-- #content -->
+		</section><!-- #primary .site-content -->
+
+<?php
+get_sidebar();
+get_footer();
---
+++ content-project.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @package _s
+ * @since _s 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="grid">
+
+		<div class="grid__item  one-whole  lap-one-half  desk-two-thirds">
+			<header class="entry-header">
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+				<p><?php the_excerpt(); ?></p>
+			</header>
+		</div>
+		<div class="grid__item  one-whole  lap-one-half  desk-one-third">
+			<?php
+			if ( has_post_thumbnail() ) {
+				the_post_thumbnail( 'medium' );
+			}
+			?>
+		</div>
+	</div>
+
+	<div class="entry-content">
+		<?php
+		the_content();
+		wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', '_s' ), 'after' => '</div>' ) );
+		?>
+	</div><!-- .entry-content -->
+
+
+	<footer class="entry-meta">
+		<?php
+
+		$post_type = get_post_type();
+		foreach ( get_object_taxonomies( $post_type ) as $tax_name ) {
+		    $term_list = get_the_term_list( get_the_ID(), $tax_name, '', ' ', '' );
+			if ( !empty( $term_list ) ) {
+				$the_tax = get_taxonomy( $tax_name );
+				?>
+				<span class="<?php echo $tax_name; ?>-links">
+					<?php printf( __( '%1$s: %2$s', '_s' ), $the_tax->labels->name, $term_list ); ?>
+				</span><br>
+				<?php
+			}
+		}
+
+		$meta_text = __( 'Bookmark the <a href="%1$s" title="Permalink to %2$s" rel="bookmark">permalink</a>.', '_s' );
+
+		printf(
+			$meta_text,
+			get_permalink(),
+			the_title_attribute( 'echo=0' )
+		);
+		?>
+
+		<?php edit_post_link( __( 'Edit', '_s' ), '<span class="edit-link">', '</span>' );
+		if ( shortcode_exists( 'followbutton' ) ) {
+			?>
+		<p><?php echo do_shortcode("[followbutton username='Tarendai' count='true' lang='en' theme='light']"); ?></p>
+			<?php
+		}
+		?>
+	</footer><!-- .entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
