a051cf793124e65354cc22a9da077a26daafe798
Tom J Nowell <contact@tomjn.com>
2013-09-03 11:16:12 +0100

    dont show private or protected on titles

--- functions.php
+++ functions.php
@@ -280,3 +280,22 @@ if ( ! function_exists( 'shortcode_exists' ) ) {
 if ( function_exists( 'add_taxonomy_templating_support' ) ) {
 	add_taxonomy_templating_support( 'category' );
 }
+
+function the_title_trim( $title ) {
+
+	$title = attribute_escape( $title );
+
+	$findthese = array(
+		'#Protected:#',
+		'#Private:#'
+	);
+
+	$replacewith = array(
+		'', // What to replace "Protected:" with
+		'' // What to replace "Private:" with
+	);
+
+	$title = preg_replace( $findthese, $replacewith, $title );
+	return $title;
+}
+add_filter( 'the_title', 'the_title_trim' );
