12da3f5758057777f033308800bc1b634644b74d
Tom J Nowell <contact@tomjn.com>
2013-09-03 11:18:11 +0100

    replace prev commit with a much cheaper function

--- functions.php
+++ functions.php
@@ -281,21 +281,8 @@ if ( function_exists( 'add_taxonomy_templating_support' ) ) {
 	add_taxonomy_templating_support( 'category' );
 }
 
-function the_title_trim( $title ) {
-
-	$title = attribute_escape( $title );
-
-	$findthese = array(
-		'#Protected:#',
-		'#Private:#'
-	);
-
-	$replacewith = array(
-		'', // What to replace "Protected:" with
-		'' // What to replace "Private:" with
-	);
-
-	$title = preg_replace( $findthese, $replacewith, $title );
-	return $title;
+function title_format( $content ) {
+	return '%s';
 }
-add_filter( 'the_title', 'the_title_trim' );
+add_filter( 'private_title_format', 'title_format' );
+add_filter( 'protected_title_format', 'title_format' );
