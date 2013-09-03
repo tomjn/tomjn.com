2bcdad1761136cf982c150dd3c0ac285c0ba7e7b
tarendai <tarendai@darkstars.co.uk>
2013-09-03 21:08:16 +0100

    add an excerpt view

--- archive.php
+++ archive.php
@@ -97,7 +97,7 @@ if ( have_posts() ) {
 		 * If you want to overload this in a child theme then include a file
 		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 		 */
-		get_template_part( 'content', get_post_type() );
+		get_template_part( 'excerpt', get_post_type() );
 	}
 	_s_content_nav( 'nav-below' );
 
---
+++ excerpt.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * @package _s
+ * @since _s 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', '_s' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
+
+		<?php if ( 'post' == get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php _s_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php endif; ?>
+	</header><!-- .entry-header -->
+
+<?php
+if ( is_search() || is_archive() ) {
+	// Only display Excerpts for Search and archives
+	?>
+	<div class="entry-summary">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-summary -->
+	<?php
+} else {
+		?>
+	<div class="entry-content">
+		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', '_s' ) ); ?>
+		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', '_s' ), 'after' => '</div>' ) ); ?>
+	</div><!-- .entry-content -->
+	<?php
+}
+?>
+	<footer class="entry-meta">
+<?php
+$post_type = get_post_type();
+foreach ( get_object_taxonomies( $post_type ) as $tax_name ) {
+    $term_list = get_the_term_list( get_the_ID(), $tax_name, '', ' ', '' );
+	if ( !empty( $term_list ) ) {
+		$the_tax = get_taxonomy( $tax_name );
+		?>
+		<span class="<?php echo $tax_name; ?>-links">
+			<?php printf( __( '%1$s: %2$s<br>', '_s' ), $the_tax->labels->name, $term_list ); ?>
+		</span>
+		<?php
+	}
+}
+?>
+
+		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', '_s' ), __( '1 Comment', '_s' ), __( '% Comments', '_s' ) ); ?></span>
+		<?php endif; ?>
+
+		<?php edit_post_link( __( 'Edit', '_s' ), ' <span class="edit-link">', '</span>' ); ?>
+	</footer><!-- #entry-meta -->
+</article><!-- #post-<?php the_ID(); ?> -->
