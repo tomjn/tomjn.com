1fa260707205ecbe2eb567d38f0f7757fa1224c8
tarendai <tarendai@darkstars.co.uk>
2013-09-03 21:10:54 +0100

    add proper project excerpt

--- excerpt-project.php
+++ excerpt-project.php
@@ -1,1 +1,11 @@
-excerpt-project.php
+<a href="<?php echo get_permalink(); ?>" class="project_listing_item grid__item one-whole palm-one-whole lap-one-half desk-one-half">
+	<div class="grid">
+		<div class="grid__item one-third palm-one-whole lap-one-whole desk-one-third">
+			<?php the_post_thumbnail( 'medium' ); ?>
+		</div>
+		<div class="grid__item two-thirds palm-one-whole lap-one-whole desk-two-thirds">
+			<h2><?php the_title(); ?></h2>
+			<?php the_excerpt(); ?>
+		</div>
+	</div>
+</a>
