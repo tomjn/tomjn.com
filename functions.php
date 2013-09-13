64e88fc4d76b532ba1af08d76952c8c930630866
Tom J Nowell <contact@tomjn.com>
2013-09-13 16:41:35 +0100

    remove some more header tags

--- functions.php
+++ functions.php
@@ -31,8 +31,12 @@ if ( ! function_exists( 'tomjnsetup' ) ) {
 		 */
 		require_once( get_template_directory() . '/inc/template-tags.php' );
 
-		remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
-		remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+		remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+		remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+		remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+		remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+		remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+		remove_action( 'wp_head', 'wp_generator' ); // Display the XHTML generator that is generated on the wp_head hook, WP version
 
 		/**
 		 * Make theme available for translation
