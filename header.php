2ee8168d19d9a02dcea912ae9a32bf4a7a6f0a19
Tom J Nowell <contact@tomjn.com>
2013-09-13 15:17:44 +0100

    A little cleanup

--- header.php
+++ header.php
@@ -12,37 +12,11 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width" />
-	<title><?php
-wp_title( '|', true, 'right' );/*
-	/*
-	 * Print the <title> tag based on what is being viewed.
-	 *//*
-	global $page, $paged;
-
-	wp_title( '|', true, 'right' );
-
-	// Add the blog name.
-	bloginfo( 'name' );
-
-	// Add the blog description for the home/front page.
-	$site_description = get_bloginfo( 'description', 'display' );
-	if ( $site_description && ( is_home() || is_front_page() ) )
-		echo " | $site_description";
-
-	// Add a page number if necessary:
-	if ( $paged >= 2 || $page >= 2 )
-		echo ' | ' . sprintf( __( 'Page %s', 'tomjn' ), max( $paged, $page ) );*/
-
-	?></title>
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-
 	<?php wp_head(); ?>
 </head>
-
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
 		<?php do_action( 'before' ); ?>
@@ -60,6 +34,6 @@ wp_title( '|', true, 'right' );/*
 					<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 				</nav>
 			</div>
-		</header><!-- #masthead .site-header -->
+		</header>
 
 		<div id="main">
