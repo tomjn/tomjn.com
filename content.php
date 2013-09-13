adb3e05453854d0e335e28542dced7e75a312631
Tom J Nowell <contact@tomjn.com>
2013-09-13 15:19:58 +0100

    more cleanup

--- content.php
+++ content.php
@@ -7,14 +7,19 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', '_s' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php _s_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+		<h1 class="entry-title">
+		<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', '_s' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+		</h1>
+		<?php
+		if ( 'post' == get_post_type() ) {
+			?>
+			<div class="entry-meta">
+				<?php _s_posted_on(); ?>
+			</div>
+			<?php
+		}
+		?>
+	</header>
 
 <?php
 if ( is_search() || is_archive() ) {
@@ -22,14 +27,14 @@ if ( is_search() || is_archive() ) {
 	?>
 	<div class="entry-summary">
 		<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
+	</div>
 	<?php
 } else {
-		?>
+	?>
 	<div class="entry-content">
 		<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', '_s' ) ); ?>
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', '_s' ), 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
+	</div>
 	<?php
 }
 ?>
@@ -47,12 +52,13 @@ foreach ( get_object_taxonomies( $post_type ) as $tax_name ) {
 		<?php
 	}
 }
+
+if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) {
+	?>
+	<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', '_s' ), __( '1 Comment', '_s' ), __( '% Comments', '_s' ) ); ?></span>
+	<?php
+}
+edit_post_link( __( 'Edit', '_s' ), ' <span class="edit-link">', '</span>' );
 ?>
-
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', '_s' ), __( '1 Comment', '_s' ), __( '% Comments', '_s' ) ); ?></span>
-		<?php endif; ?>
-
-		<?php edit_post_link( __( 'Edit', '_s' ), ' <span class="edit-link">', '</span>' ); ?>
-	</footer><!-- #entry-meta -->
-</article><!-- #post-<?php the_ID(); ?> -->
+	</footer>
+</article>
