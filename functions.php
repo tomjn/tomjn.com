0028638dd267dd0ecd293234d635c16bc69276d5
Tom J Nowell <contact@tomjn.com>
2013-09-13 16:33:43 +0100

    remove unused fittext reference and remove keyboard nav

--- functions.php
+++ functions.php
@@ -149,14 +149,9 @@ function tomjnscripts() {
 	wp_enqueue_style( 'lessstyle', get_template_directory_uri().'/style.less',array(),'5' );
 
 	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
-	wp_enqueue_script( 'fittext', get_template_directory_uri() . '/js/jquery.fittext.js', array( 'jquery' ), '1.1' );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
-	}
-
-	if ( is_singular() && wp_attachment_is_image( $post->ID ) ) {
-		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'tomjnscripts' );
