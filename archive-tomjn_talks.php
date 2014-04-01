2e180df976078f65fcf4762800903165e4dcc973
Tom J Nowell <contact@tomjn.com>
2014-04-01 22:23:10 +0100

    Create archive-tomjn_talks.php

---
+++ archive-tomjn_talks.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * The template for displaying Taxonomy pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package _s
+ * @since _s 1.0
+ */
+
+get_header();
+$taxonomy = '';
+if ( is_tax() ) {
+	$taxonomy = get_query_var( 'taxonomy' );
+} else {
+	if ( is_category() ) {
+		$taxonomy = 'category';
+	} else if ( is_tag() ) {
+		$taxonomy = 'post_tag';
+	}
+}
+
+$current_term =	$wp_query->queried_object;
+$tname = $taxonomy;
+$taxobj = get_taxonomy( $taxonomy );
+if ( !empty( $taxobj ) ) {
+	$tname = $taxobj->labels->name;
+}
+?>
+		<section id="primary" class="site-content">
+			<div id="content" role="main">
+<?php
+if ( have_posts() ) {
+	?>
+				<header class="page-header">
+
+	<?php
+	echo '<h1>Projects &amp; Work</h1>';
+	?>
+				</header>
+
+					<div class="taxonomy-listing grid">
+	<?php
+
+	/* Start the Loop */
+	while ( have_posts() ) {
+		the_post();
+
+		?>
+		<a href="<?php echo get_permalink(); ?>" class="project_listing_item grid__item one-whole palm-one-whole lap-one-half desk-one-half">
+			<div class="grid">
+				<div class="grid__item one-third palm-one-whole lap-one-whole desk-one-third">
+					<?php the_post_thumbnail( 'project-main' ); ?>
+				</div>
+				<div class="grid__item two-thirds palm-one-whole lap-one-whole desk-two-thirds">
+					<h2><?php the_title(); ?></h2>
+					<?php the_excerpt(); ?>
+				</div>
+			</div>
+		</a>
+		<?php
+	}
+	_s_content_nav( 'nav-below' );
+
+} else {
+	get_template_part( 'no-results', 'archive' );
+}
+?>
+					</div>
+				</div>
+			</div>
+		</section>
+<?php
+get_sidebar();
+get_footer();
