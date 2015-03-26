bb2912462dd96207dc82b2adb2a04f240e42a071
Tom J Nowell <contact@tomjn.com>
2015-03-26 19:57:52 +0000

    swap taxonomy columns

--- taxonomy.php
+++ taxonomy.php
@@ -26,6 +26,21 @@ if ( !empty( $taxobj ) ) {
 					<h1><?php echo esc_html( $current_term->name ) ?></h1>
 				</header>
 				<div class="taxonomy-list grid">
+					<div class="taxonomy-listing grid__item three-quarters palm-one-whole lap-two-thirds desk-three-quarters">
+						<?php
+
+						/* Start the Loop */
+						while ( have_posts() ) {
+							the_post();
+							get_template_part( 'excerpt', get_post_type() );
+						}
+						_s_content_nav( 'nav-below' );
+
+					} else {
+						get_template_part( 'no-results', 'archive' );
+					}
+					?>
+					</div>
 					<div class="taxonomy-term-choices grid__item one-quarter palm-one-whole lap-one-third desk-one-quarter">
 						<h1 class="page-title"><?php echo esc_html( $tname ); ?></h1>
 						<?php
@@ -42,21 +57,6 @@ if ( !empty( $taxobj ) ) {
 							echo '</ul>';
 						}
 						?>
-					</div>
-					<div class="taxonomy-listing grid__item three-quarters palm-one-whole lap-two-thirds desk-three-quarters">
-						<?php
-
-						/* Start the Loop */
-						while ( have_posts() ) {
-							the_post();
-							get_template_part( 'excerpt', get_post_type() );
-						}
-						_s_content_nav( 'nav-below' );
-
-					} else {
-						get_template_part( 'no-results', 'archive' );
-					}
-					?>
 					</div>
 				</div>
 			</div>
