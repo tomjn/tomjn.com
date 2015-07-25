72794cb36832a1581662a369775b3de8aedd2198
Tom J Nowell <contact@tomjn.com>
2015-07-25 15:43:21 +0100

    remove the colophon

--- footer.php
+++ footer.php
@@ -2,26 +2,13 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the id=main div and all content after
- *
- * @package _s
- * @since _s 1.0
+ * @package tomjn.com
  */
 ?>
 	</div>
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="footer-container">
-			<div class="site-info">
-				<?php do_action( '_s_credits' ); ?>
-			</div>
-		</div>
-	</footer>
 </div>
-
 <?php
 wp_footer();
 ?>
-
 </body>
 </html>
