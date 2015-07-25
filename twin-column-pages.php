18997a6ac9f4db0433ace61c0ea7937606d6dcd1
Tom J Nowell <contact@tomjn.com>
2015-07-25 14:18:39 +0100

    add a template header comment

--- twin-column-pages.php
+++ twin-column-pages.php
@@ -1,14 +1,8 @@
 <?php
 /**
- * The template for displaying all pages.
+ * Template Name: Twin column
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
- *
- * @package _s
- * @since _s 1.0
+ * @package tomjn.com
  */
 
 get_header();
