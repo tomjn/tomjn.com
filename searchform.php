0de209b21f668c70b2c06b526e1a125e87ae487c
Tom J Nowell <contact@tomjn.com>
2015-07-28 18:08:21 +0100

    fix a fatal error in the search form php

--- searchform.php
+++ searchform.php
@@ -8,6 +8,6 @@
 ?>
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<label for="s" class="assistive-text"><?php _e( 'Search', '_s' ); ?></label>
-	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" value="<?php if ( !empty( $_GET['s'] ) { echo esc_attr( $_GET['s'] ); } ?>" />
+	<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" value="<?php if ( !empty( $_GET['s'] ) ) { echo esc_attr( $_GET['s'] ); } ?>" />
 	<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '_s' ); ?>" />
 </form>
