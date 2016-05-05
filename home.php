3870954bb8d05896bac37786da9e50f47fe7055e
Tom J Nowell <contact@tomjn.com>
2016-05-05 17:03:44 +0100

    Remove the post loop, there are no posts here anyway

--- home.php
+++ home.php
@@ -1,17 +1,10 @@
 <?php
-
 get_header();
 ?>
 		<div id="primary" class="site-content">
 			<div id="content" role="main">
 				<?php
-				while ( have_posts() ) {
-					the_post();
-					get_template_part( 'content', 'home' );
-					if ( have_comments() || comments_open() ) {
-						comments_template( '', true );
-					}
-				}
+				get_template_part( 'content', 'home' );
 				?>
 			</div>
 		</div>
