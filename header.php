ebe665866bdbeac9c2e479411339fe5f93f8855c
Tom J Nowell <contact@tomjn.com>
2016-05-05 16:06:01 +0100

    Use get_bloginfo instead of bloginfo

--- header.php
+++ header.php
@@ -10,10 +10,10 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta charset="<?php echo esc_attr( get_bloginfo( 'charset' ) ); ?>" />
 	<meta name="viewport" content="width=device-width" />
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link rel="pingback" href="<?php echo esc_attr( get_bloginfo( 'pingback_url' ) ); ?>" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
