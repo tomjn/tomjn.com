8350d4a84da6a6801aa05feec5ee430faa8c6f8c
Tom J Nowell <contact@tomjn.com>
2016-05-09 11:58:54 -0400

    remove the transient as it made things slower not faster, and fix the transient staleness check

--- functions.php
+++ functions.php
@@ -335,8 +335,9 @@ function _tomjn_home_cancel_query( $query, \WP_Query $q ) {
 add_filter( 'posts_request', '_tomjn_home_cancel_query', 100, 2 );
 
 function tomjn_get_the_term_list( $id, $taxonomy, $before, $sep, $after ) {
+	return get_the_term_list( $id, $taxonomy, $before, $sep, $after );
 	$result = get_transient( 'tomjn_get_the_term_list_'.$id.'_'.$taxonomy );
-	if ( !$result ) {
+	if ( false === $result ) {
 		$result = get_the_term_list( $id, $taxonomy, $before, $sep, $after );
 		if ( $result ) {
 			set_transient( 'tomjn_get_the_term_list_'.$id.'_'.$taxonomy , $result, 60 * 60 * 24 );
