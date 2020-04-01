40fc3e26b193225bafd59aea5cded771e2c0a122
tomjn <contact@tomjn.com>
2020-04-01 17:02:49 +0100

    simplify the homepage template

--- home.php
+++ home.php
@@ -3,9 +3,29 @@ get_header();
 ?>
 		<div id="primary" class="site-content">
 			<div id="content" role="main">
-				<?php
-				get_template_part( 'content', 'home' );
-				?>
+				<article <?php post_class(); ?>>
+					<div id="twin-columns" class="grid">
+						<?php
+						if ( is_active_sidebar( 'sidebar-home-top' ) ) {
+							?>
+							<div class="grid__item  one-whole">
+								<?php dynamic_sidebar('sidebar-home-top'); ?>
+							</div>
+							<?php
+						}
+						if ( is_active_sidebar( 'sidebar-home-left' ) || is_active_sidebar( 'sidebar-home-right' ) ) {
+							?>
+							<div class="grid__item  one-whole palm-one-whole lap-one-half  desk-one-half">
+								<?php dynamic_sidebar('sidebar-home-left'); ?>
+							</div>
+							<div class="grid__item  one-whole palm-one-whole lap-one-half  desk-one-half">
+								<?php dynamic_sidebar('sidebar-home-right'); ?>
+							</div>
+							<?php
+						}
+						?>
+					</div>
+				</article>
 			</div>
 		</div>
 <?php
